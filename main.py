52ef70a27a4c93a3c2d2ab640b3f213b5e747c69
Hector Mcfoy <learnwithhector@gmail.com>
2022-02-23 15:08:50 +0000

    blind auction - no error handling if <int> not entered for bids

---
+++ main.py
@@ -0,0 +1,38 @@
+from art import logo
+import os
+
+print(logo)
+
+bidder1_name = input("You are the first bidder. What is your name? ")
+bidder1_amount = int(input("How much do you bid $"))
+
+auction = {}
+
+auction[bidder1_name] = bidder1_amount
+
+more_bidders = ''
+while more_bidders != 'no' and more_bidders != 'n' and more_bidders != 'yes' and more_bidders != 'y':
+    more_bidders = input("Are there any other bidders? type 'yes' or 'no'\n").casefold()
+
+more_bidders = True if more_bidders == 'y' or more_bidders == 'yes' else False
+
+while more_bidders:
+    os.system('clear')
+    name = input("Hello next bidder. What is your name?: ")
+    amount = int(input("What is your bid?: $"))
+    auction[name] = amount
+    while more_bidders != 'no' and more_bidders != 'n' and more_bidders != 'yes' and more_bidders != 'y':
+        more_bidders = input("Are there any other bidders? type 'yes' or 'no'\n").casefold()
+
+    more_bidders = True if more_bidders == 'y' or more_bidders == 'yes' else False
+
+
+# Find highest bid
+highest_bid = 0
+
+for k in auction:
+    if auction[k] > highest_bid:
+        highest_bid = auction[k]
+        highest_bidder = k
+
+print(f"The winner is {highest_bidder} with a bid of ${highest_bid}.")
